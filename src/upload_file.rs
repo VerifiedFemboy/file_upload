002293656de330d6eab478be97b3c47793d17f53
VerifiedFemboy <maciekplaygamesyt@gmail.com>
2024-05-25 10:33:47 +0200

    [+] Uploaded files list

--- src/upload_file.rs
+++ src/upload_file.rs
@@ -1,13 +1,14 @@
 use std::fs as fs;
 use std::io::Write;
 use std::path::Path;
-use actix_files::NamedFile;
 
+use actix_files::NamedFile;
 use actix_multipart::Multipart;
 use actix_web::{get, HttpResponse, post, web};
 use futures_util::{StreamExt, TryStreamExt};
 
 const DIRECTORY: &str = "./upload";
+const STATIC_DIR: &str = "./static";
 
 #[post("/upload")]
 pub async fn upload_post(mut payload: Multipart) -> HttpResponse {
@@ -36,23 +37,47 @@ pub async fn list_files() -> HttpResponse {
     match fs::read_dir(DIRECTORY) {
         Ok(entries) => {
             let mut file_links = String::new();
+
             for entry in entries.filter_map(|entry| entry.ok()) {
                 if let Some(filename) = entry.path().file_name().and_then(|name| name.to_str()) {
-                    file_links.push_str(&format!("<li><a href=\"/uploads/{}\">{}</a></li>", filename, filename));
+                    if let Some(extension) = entry.path().extension().and_then(|ext| ext.to_str()) {
+                        match extension {
+                            "png" | "jpg" | "jpeg" | "gif" => {
+                                file_links.push_str(&format!(
+                                    "<a href=\"/uploads/{}\" target=\"_blank\"><img src=\"/uploads/{}\" alt=\"{}\" style=\"width:500px;height:auto;\" /></a>",
+                                    filename, filename, filename
+                                ));
+                            },
+                            "mp4" | "webm" | "ogg" => {
+                                file_links.push_str(&format!(
+                                    "<video width=\"500\" height=\"auto\" controls><source src=\"/uploads/{}\" type=\"video/{}\">Your browser does not support the video tag.</video>",
+                                    filename, extension
+                                ));
+                            },
+                            _ => {
+                                file_links.push_str(&format!(
+                                    "<a href=\"/uploads/{}\">{}</a>",
+                                    filename, filename
+                                ));
+                            }
+                        }
+                    } else {
+                        file_links.push_str(&format!(
+                            "<a href=\"/uploads/{}\">{}</a>",
+                            filename, filename
+                        ));
+                    }
                 }
             }
 
-            let html = format!(
-                "<html><head><title>Uploaded Files</title></head><body><h1>Uploaded Files</h1><ul>{}</ul></body></html>",
-                file_links
-            );
+            let file_path = format!("{STATIC_DIR}/uploads.html");
+            let html_contents = fs::read_to_string(file_path);
 
-            HttpResponse::Ok()
-                .content_type("text/html; charset=utf-8")
-                .body(html)
-        },
+            let html = html_contents.unwrap().replace("{}", &file_links);
+            HttpResponse::Ok().content_type("text/html; charset=utf-8").body(html)
+        }
         Err(err) => {
-            HttpResponse::InternalServerError().body(format!("Error reading directory: {}", err))
+            HttpResponse::InternalServerError().body(format!("Error reading dir: {}", err))
         }
     }
 }
